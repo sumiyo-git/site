4c1af6a7c6a63f2e1875d991b3fc6f94d7517c84
Dragoft <16841977@qq.com>
2025-05-23 00:07:25 +0800

    UPDATE

--- functions/api/comments.js
+++ functions/api/comments.js
@@ -24,7 +24,7 @@
 
 	// 添加留言
 	if (m == "1") {
-		if (body.content.length > 200 || body.name.length > 16) {return Response.json(r)}
+		if (body.content.length > 200 || body.name.length > 20) {return Response.json(r)}
 
 		// 生成一个 id
 		var now = new Date()
@@ -33,7 +33,7 @@
 		var parts = formatter.formatToParts(now).reduce((acc, part) => ({ ...acc, [part.type]: part.value }), {})
 		var id = `${parts.year}-${parts.month}-${parts.day} ${parts.hour}:${parts.minute}:${parts.second}`
 
-		await context.env.MetaDB.prepare('INSERT INTO pool (id, op, name, content, reply) VALUES (?, ?, ?, ?, ?)').bind(id, '0', body.name.replace(/\n/g, ''), body.content, '[]').first()
+		await context.env.MetaDB.prepare('INSERT INTO pool (id, op, name, content) VALUES (?, ?, ?, ?)').bind(id, '0', body.name.replace(/\n/g, ''), body.content).first()
 		await context.env.MetaDB.prepare('UPDATE root set data=data+1 where name="comment"').first()
 
 		r.success = true
@@ -51,6 +51,10 @@
 		r = await context.env.MetaDB.prepare('SELECT * from root where name="comment"').all()
 		r.msg = null
 	}
+
+
+
+	return Response.json(r)
 }
 
 

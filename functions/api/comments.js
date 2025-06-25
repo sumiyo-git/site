67eb7e12a6f955744e3854001c7c715c672b16f1
Dragoft <16841977@qq.com>
2025-06-26 00:23:00 +0800

    UPDATE

--- functions/api/comments.js
+++ functions/api/comments.js
@@ -61,7 +61,7 @@
 		// 生成 id 时间戳
 		var f = new Intl.DateTimeFormat('en-US', {timeZone: 'Asia/Shanghai', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false})
 		var t = f.formatToParts(new Date()).reduce((acc, part) => ({ ...acc, [part.type]: part.value }), {})
-		var id = `${t.year}-${t.month}-${t.day} ${t.hour}:${t.minute}:${t.second}`.replace(/:/g, '').replace(/-/g, '').replace(/ /g, '')
+		var id = `${t.year}-${t.month}-${t.day} ${t.hour}:${t.minute}:${t.second}`
 
 		if (body.id == null) {
 			// 留言

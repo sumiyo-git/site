c808e1c6edccca0aedeb3bb67205956acf97c798
Dragoft <16841977@qq.com>
2025-08-20 18:31:20 +0800

    UPDATE

--- functions/api/token.js
+++ functions/api/token.js
@@ -1,5 +1,18 @@
 export async function onRequest(context) {
 	var f = new Intl.DateTimeFormat('en-US', {timeZone: 'Asia/Shanghai', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false})
 	var t = f.formatToParts(new Date()).reduce((acc, part) => ({ ...acc, [part.type]: part.value }), {})
-	return Response.json((t.year * t.month + t.day).slice(-6))
+	var k = (t.year * t.month + t.day).slice(-6)
+
+    const html = `
+    	<!DOCTYPE html>
+		<body>
+			<div>${k}</div>
+		</body>
+	`
+
+    return new Response(html, {
+		headers: {
+			"content-type": "text/html;charset=UTF-8",
+		},
+    })
 }

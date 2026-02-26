8b20ec38e03ad3067da506fd002c7d1268bba055
sumiyo <mio@sumiyo.link>
2026-02-26 09:47:36 +0800

    UPDATE

--- js/root.js
+++ js/root.js
@@ -400,7 +400,6 @@ env.f.root.theme = function() {
 
 // 初始化
 if (env.d.isNetwork) {
-	if (document.domain != env.d.domain) {window.location.href = `https://${env.d.domain}`}
 	(env.f.root.cookie.get()?.startsWith("{") ?? false) ? null : env.f.root.cookie("{}")
 }
 
